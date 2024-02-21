a088a4860aaa81ff9c120bbefe16ad71bd423a12
valencsoares <pvcampossoares558@gmail.com>
2024-02-21 17:22:56 -0400

    Feito o componente de rodapé

---
+++ src/app/componentes/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return (
+        <footer>
+            <div>
+                <ul>
+                    <li>rodapé</li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
