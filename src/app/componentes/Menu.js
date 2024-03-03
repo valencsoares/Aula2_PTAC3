d0b758a6dc27f397f92869a440edae3de2e86c34
valencsoares <pvcampossoares558@gmail.com>
2024-03-03 17:36:16 -0400

    Criado o Link com o href direcionado ao site do moodle, com a Image como filho do Link

--- src/app/componentes/Menu.js
+++ src/app/componentes/Menu.js
@@ -5,7 +5,12 @@ import Image from "next/image";
 export default function Menu(){
     return (
         <header className={styles.cabecalho}>
-            <h1>Menu</h1>
+            <div>
+                <Link href="ead.ifms.edu.br">
+                    <Image width={300} height={100} src={'https://ead.ifms.edu.br/pluginfile.php/1/theme_moove/logo/1699458447/banner%20topo%20capa%20sem%20brasao.jpg'}/>
+                </Link>
+            </div>
+            <h1>Menu!</h1>
             <nav>
                 <ul>
                     <Link href="/">
