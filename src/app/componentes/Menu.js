f1bb16c2494d9c1f6cbeaeea14b3bfe92e622b4b
valencsoares <pvcampossoares558@gmail.com>
2024-03-06 16:59:59 -0400

    Corrigido o link para poder ir direto para o site do Moodle ao clicar na imagem

--- src/app/componentes/Menu.js
+++ src/app/componentes/Menu.js
@@ -6,11 +6,10 @@ export default function Menu(){
     return (
         <header className={styles.cabecalho}>
             <div>
-                <Link href="ead.ifms.edu.br">
+                <Link href="https://ead.ifms.edu.br">
                     <Image width={300} height={100} src={'https://ead.ifms.edu.br/pluginfile.php/1/theme_moove/logo/1699458447/banner%20topo%20capa%20sem%20brasao.jpg'}/>
                 </Link>
             </div>
-            <h1>Menu!</h1>
             <nav>
                 <ul className={styles.link}>
                     <Link href="/" >
